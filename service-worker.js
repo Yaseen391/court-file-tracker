0a09f5a6f1dc778da6b49064b8d1da98e3cfcbd3
Yaseen391 <yaseenpahore391@gmail.com>
2025-05-10 20:58:46 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -31,10 +31,8 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }).catch(() => {
-      return caches.match("./index.html");
-    })
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    ).catch(() => caches.match("./index.html"))
   );
 });
