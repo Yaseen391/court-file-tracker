1dc08de04605e13c603dd1bb49d1abf27edb0ae3
Yaseen391 <yaseenpahore391@gmail.com>
2025-05-28 13:59:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1728,7 +1728,7 @@ function showAnalytics() {
 // Service Worker Registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
       console.log('Service Worker registered:', registration);
     }).catch(error => {
       console.error('Service Worker registration failed:', error);
